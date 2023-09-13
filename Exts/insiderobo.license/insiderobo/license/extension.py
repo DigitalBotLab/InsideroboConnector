64a21c252457fdc5b2114f979ce4a69cc263da06
yizhouzhao <yizhou_zhao@berkeley.edu>
2023-09-13 10:55:08 -0700

    add licenser

--- Exts/insiderobo.license/insiderobo/license/extension.py
+++ Exts/insiderobo.license/insiderobo/license/extension.py
@@ -1,5 +1,12 @@
 import omni.ext
 import omni.ui as ui
+import omni.usd
+
+from pxr import Sdf
+import os
+# import omni.kit.window.file
+
+from .params import LICENSE2PATH
 
 
 # Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
@@ -19,12 +26,55 @@ class InsideroboLicenseExtension(omni.ext.IExt):
 
         self._count = 0
 
-        self._window = ui.Window("My Window", width=300, height=300)
+        self._window = ui.Window("Insiderobo License", width=300, height=300)
         with self._window.frame:
             with ui.VStack():
-                with ui.HStack():
-                    ui.Button("Add", clicked_fn=self.debug)
+                with ui.HStack(height = 20):
+                    ui.Label("Prim Path:", width = 100)
+                    self.prim_path_ui = ui.StringField()
+                with ui.HStack(height = 20):
+                    ui.Label("License Name:", width = 100)
+                    self.license_name_ui = ui.StringField()
+                    self.license_name_ui.model.set_value("kinova")
+                with ui.HStack(height = 20):
+                    ui.Label("License Path:", width = 100)
+                    self.license_path_ui = ui.StringField()
+                    self.license_name = self.license_name_ui.model.get_value_as_string()
+                    self.license_path_ui.model.set_value(LICENSE2PATH[self.license_name])  
+                
+                ui.Button("Add License to Prim", height = 20, clicked_fn=self.add_license)
+                
     
+    def add_license(self):
+        print("adding license")
+        stage = omni.usd.get_context().get_stage()
+        prim_path = self.prim_path_ui.model.get_value_as_string()
+        
+        # if the prim path is empty, use the default prim path
+        if prim_path == "":
+            prim_path = stage.GetDefaultPrim().GetPath().pathString
+
+        license_path = self.license_path_ui.model.get_value_as_string()
+        prim = stage.GetPrimAtPath(prim_path)
+
+        # load the license file into string
+        license_file = open(license_path, "r")
+        license_text = license_file.read()
+
+        print("license text: ", license_text)
+        
+        attribute_name = f"{self.license_name}_license"
+        if not prim.HasAttribute(attribute_name):
+            # create a new attribute on the prim
+            prim.CreateAttribute(attribute_name, Sdf.ValueTypeNames.String, False).Set(license_text)
+        
+        # save the stage
+        omni.usd.get_context().get_stage().Save()
+
+        license_file.close()
+        
+
+                    
     def debug(self):
         print("[insiderobo.license] insiderobo license debug: ")
 
---
+++ Exts/insiderobo.license/insiderobo/license/params.py
@@ -0,0 +1,5 @@
+# parameters
+
+LICENSE2PATH = {
+    "kinova": "I:\\DBL\\InsideroboConnector\\Exts\\insiderobo.license\\insiderobo\\license\\licences\\KINOVA_LICENSE.txt",
+}
